20db6963e55b4eef860f9845c9a19a28d667ea48
Nguyen Dinh Tuan Anh <ndtatuananh@gmail.com>
2023-02-15 22:14:02 +0700

    Add contents to navbar & footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,8 @@
-import { Box, Container } from "@mui/material";
+import { Box, Container, Divider, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { BsTelephoneFill, BsMailbox2, BsLinkedin } from "react-icons/bs";
 
 export default function Footer() {
   return (
@@ -11,7 +14,58 @@ export default function Footer() {
       }}
     >
       <Container maxWidth="xl">
-        <Box sx={{ p: { xs: 1, md: 3 } }}>Footer</Box>
+        <Box sx={{ p: { xs: 1, md: 3 } }}>
+          <Stack direction={"row"} gap={2} justifyContent={"space-between"}>
+            <Stack>
+              <Typography
+                variant="caption"
+                sx={{ display: "flex", alignItems: "center", gap: 1 }}
+              >
+                <BsTelephoneFill fontSize={12} /> (+84) 39 8938 320
+              </Typography>
+              <Typography
+                variant="caption"
+                sx={{ display: "flex", alignItems: "center", gap: 1 }}
+              >
+                <BsMailbox2 fontSize={12} /> ndtatuananh@gmail.com
+              </Typography>
+              <Link href={"https://www.linkedin.com/in/ndtatuananh/"}>
+                <Typography
+                  variant="caption"
+                  sx={{ display: "flex", alignItems: "center", gap: 1 }}
+                >
+                  <BsLinkedin fontSize={12} /> ndtatuananh
+                </Typography>
+              </Link>
+              <Typography variant="caption" sx={{ mt: 2 }}>
+                Feel free to contact if you want to work with me
+              </Typography>
+            </Stack>
+            <Stack alignItems={"center"}>
+              <Image
+                alt="footer logo"
+                src="/img/GameFluence-black-250px.png"
+                width={150}
+                height={75}
+              />
+              <Typography variant="caption">
+                I made this website mainly to hone my Front-end skills
+                (〜￣▽￣)〜
+              </Typography>
+            </Stack>
+            <Stack>
+              <Typography variant="caption">
+                Made by Anh Nguyen with 💕
+              </Typography>
+              <Typography variant="caption">
+                Data & API provided by{" "}
+                <Link href={"https://rawg.io"} className="font-semibold">
+                  RAWG
+                </Link>
+              </Typography>
+            </Stack>
+          </Stack>
+        </Box>
       </Container>
     </Box>
   );
--- components/Navigation/NavDesktop.js
+++ components/Navigation/NavDesktop.js
@@ -1,20 +1,44 @@
-import { Stack } from "@mui/material";
+import LoginIcon from "@mui/icons-material/Login";
+import { Button, Divider, Stack } from "@mui/material";
+import dynamic from "next/dynamic";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+
+const SearchModal = dynamic(() => import("./SearchModal"), { ssr: false });
 
 export default function NavDesktop() {
   return (
-    <Stack direction={"row"}>
+    <Stack
+      alignItems={"center"}
+      direction={"row"}
+      gap={1}
+      justifyContent={"space-between"}
+      sx={{ width: "100%" }}
+    >
       <Link href={"/"}>
         <Image
           alt="logo"
-          loading="lazy"
           src="/img/GameFluence-black-250px.png"
           width={150}
           height={75}
         />
       </Link>
+      <Stack
+        alignItems={"center"}
+        direction={"row"}
+        divider={<Divider orientation="vertical" flexItem />}
+        gap={1}
+      >
+        <SearchModal />
+        <Button
+          size="small"
+          startIcon={<LoginIcon />}
+          sx={{ textTransform: "none" }}
+          // variant="outlined"
+        >
+          Login
+        </Button>
+      </Stack>
     </Stack>
   );
 }
