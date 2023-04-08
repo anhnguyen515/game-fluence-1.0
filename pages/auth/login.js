bc7ff7a230c781288f3c1313d0f5a5e4c67726b9
Nguyen Dinh Tuan Anh <ndtatuananh@gmail.com>
2023-04-08 21:32:17 +0700

    Add more images to login & signup pages

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,5 +1,6 @@
 import { loginAPI } from "@/apis/user";
 import { selectUser, setUser } from "@/store/slices/userSlice";
+import { backgroundImages } from "@/utils/constants";
 import { getRandomInt } from "@/utils/utils";
 import VisibilityOffRoundedIcon from "@mui/icons-material/VisibilityOffRounded";
 import VisibilityRoundedIcon from "@mui/icons-material/VisibilityRounded";
@@ -22,57 +23,6 @@ import { useRouter } from "next/router";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
-
-const backgroundImages = [
-  {
-    img: `/img/login_backgrounds/1.jpg`,
-    src: "https://pin.it/23f2NHO",
-  },
-  {
-    img: `/img/login_backgrounds/2.jpg`,
-    src: "https://pin.it/6jQ9LMk",
-  },
-  {
-    img: `/img/login_backgrounds/3.jpg`,
-    src: "https://pin.it/7gRv3ZL",
-  },
-  {
-    img: `/img/login_backgrounds/4.jpg`,
-    src: "https://pin.it/12fWEyN",
-  },
-  {
-    img: `/img/login_backgrounds/5.jpg`,
-    src: "https://pin.it/37q84id",
-  },
-  {
-    img: `/img/login_backgrounds/6.jpg`,
-    src: "https://pin.it/607OqFd",
-  },
-  {
-    img: `/img/login_backgrounds/7.jpg`,
-    src: "https://pin.it/12bRWUM",
-  },
-  {
-    img: `/img/login_backgrounds/8.jpg`,
-    src: "https://pin.it/41DUfB7",
-  },
-  {
-    img: `/img/login_backgrounds/9.jpg`,
-    src: "https://pin.it/5SY7qCe",
-  },
-  {
-    img: `/img/login_backgrounds/10.jpg`,
-    src: "https://pin.it/5s2G0bh",
-  },
-  {
-    img: `/img/login_backgrounds/11.jpg`,
-    src: "https://pin.it/YF8nrzj",
-  },
-  {
-    img: `/img/login_backgrounds/12.jpg`,
-    src: "https://pin.it/7MLmyVd",
-  },
-];
 
 export default function LoginPage() {
   const userStore = useSelector(selectUser);
--- utils/constants.js
+++ utils/constants.js
@@ -6,3 +6,57 @@ export const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 export const PAGINATION_LIMIT = 8;
+export const backgroundImages = [
+  {
+    img: `/img/login_backgrounds/1.jpg`,
+    src: "https://pin.it/23f2NHO",
+  },
+  {
+    img: `/img/login_backgrounds/2.jpg`,
+    src: "https://pin.it/6jQ9LMk",
+  },
+  {
+    img: `/img/login_backgrounds/3.jpg`,
+    src: "https://pin.it/7gRv3ZL",
+  },
+  {
+    img: `/img/login_backgrounds/4.jpg`,
+    src: "https://pin.it/12fWEyN",
+  },
+  {
+    img: `/img/login_backgrounds/5.jpg`,
+    src: "https://pin.it/37q84id",
+  },
+  {
+    img: `/img/login_backgrounds/6.jpg`,
+    src: "https://pin.it/607OqFd",
+  },
+  {
+    img: `/img/login_backgrounds/7.jpg`,
+    src: "https://pin.it/12bRWUM",
+  },
+  {
+    img: `/img/login_backgrounds/8.jpg`,
+    src: "https://pin.it/41DUfB7",
+  },
+  {
+    img: `/img/login_backgrounds/9.jpg`,
+    src: "https://pin.it/5SY7qCe",
+  },
+  {
+    img: `/img/login_backgrounds/10.jpg`,
+    src: "https://pin.it/5s2G0bh",
+  },
+  {
+    img: `/img/login_backgrounds/11.jpg`,
+    src: "https://pin.it/5CTfrR2",
+  },
+  {
+    img: `/img/login_backgrounds/12.jpg`,
+    src: "https://pin.it/7MLmyVd",
+  },
+  {
+    img: `/img/login_backgrounds/13.jpg`,
+    src: "https://pin.it/6WYnYU6",
+  },
+];
